f0ce3a90fd87582e39d8b8f5911ade3459ab23e1
GopiMannit <gopivijayangeetha8102@gmail.com.com>
2024-03-19 11:50:18 +0530

    zone data changed

--- assets/data/zone.js
+++ assets/data/zone.js
@@ -1,6 +1,6 @@
 // Define an array of zone values
 var zoneValues = [
-    "ADMKvsBJPvsDMKvsNTK", "DMKvsBJPvsADMKvsNTK", "BJPvsADMKvsDMKvsNTK", "DMKvsADMKvsBJPvsNTK", "ADMKvsDMKvsBJPvsNTK",
+    "ADMK vs BJP vs DMK vs NTK", "DMK vs BJP vs ADMK vs NTK", "BJP vs ADMK vs DMK vs NTK", "DMK vs ADMK vs BJP vs NTK", "ADMK vs DMK vs BJP vs NTK",
     "Consolidated"
 ];
 
