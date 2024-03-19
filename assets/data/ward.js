4f990b56237fab4fbb2f4ec512a51359a688b12e
GopiMannit <gopivijayangeetha8102@gmail.com.com>
2024-03-19 10:09:15 +0530

    first commit

---
+++ assets/data/ward.js
@@ -0,0 +1,3 @@
+var wardValues = [
+    "IVRS", "Manual","Consolidated"
+];
---
+++ assets/data/zone.js
@@ -0,0 +1,12 @@
+// Define an array of zone values
+var zoneValues = [
+    "ADMKvsBJPvsDMKvsNTK", "DMKvsBJPvsADMKvsNTK", "BJPvsADMKvsDMKvsNTK", "DMKvsADMKvsBJPvsNTK", "ADMKvsDMKvsBJPvsNTK",
+    "Consolidated"
+];
+
+// Admk vs BJP vs DMK vs NTK
+// DMk vs BJP vs ADMK vs NTK
+// BJP vs ADMK vs DMK vs NTK
+// DMK vs ADMK vs BJP vs NTK
+// ADMK vs DMK vs BJP vs NTK
+// consolidated
